04c898fb202360404f887612b7d4735050c0ab98
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-12-03 15:44:42 -0600

    error on get plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
 
+require("config.lazy")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,18 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
       colorscheme = "solarized-osaka",
     } },
-    -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -23,28 +26,39 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
-    -- import/override with your plugins
     { import = "plugins" },
+
+    -- Añadir plugins adicionales aquí
+    -- Python
+    { "psf/black", as = "black" },
+    { "pyright", as = "pyright" },
+
+    -- C#
+    { "OmniSharp/omnisharp-vim", as = "omnisharp" },
+
+    -- Java
+    { "mfussenegger/nvim-jdtls", as = "jdtls" },
+
+    -- C++
+    { "clangd", as = "clangd" },
+
+    -- SQL
+    { "sql-language-server", as = "sql-language-server" },
+
+    -- JavaScript/TypeScript
+    { "typescript-language-server", as = "typescript-language-server" },
+    { "eslint", as = "eslint" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
